4dd3a0bdf17f3d733e26b5e7c53450324fe62429
Bugra Isguzar <biisguzar@gmail.com>
2020-02-26 14:17:02 +0300

    seperate pms api

--- core/modules/read_data.py
+++ core/modules/read_data.py
@@ -1,9 +1,34 @@
 def read_data(api):
+    def fan_health():
+        response = api.getFanHealth()
+        responses = {1: True, 2: False}
+
+        return responses[response]
+
+    def working_mode():
+        response = api.getWorkingMode()
+        responses = {
+            1: "Charging",
+            2: "Fully Charged - Adapter Powered",
+            3: "Battery Powered",
+        }
+
+        return responses[response]
+
+    def watchdog_signal():
+        response = api.askWatchdogAlarm()
+        responses = {1: True, 2: False}
+
+        return responses[response]
+
     return {
         "charge_status": api.getBatteryLevel(),
         "battery_health": api.getBatteryHealt(),
         "fanspeed": api.getFanSpeed(),
-        "stats":{
+        "fan_health": fan_health(),
+        "working_status": working_mode(),
+        "watchdog_signal": watchdog_signal(),
+        "stats": {
             "input": {
                 "temperature": api.getInputTemp(),
                 "voltage": api.getInputVoltage(),
@@ -21,6 +46,6 @@ def read_data(api):
                 "voltage": api.getBatteryVoltage(),
                 "current": api.getBatteryCurrent(),
                 "power": api.getBatteryPower(),
-            }
-        }
+            },
+        },
     }
