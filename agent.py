0849faf400d8d3df8f7106fcae19034c23d8a76b
Bugra Isguzar <biisguzar@gmail.com>
2020-04-06 01:33:22 +0300

    add external config object

--- agent.py
+++ agent.py
@@ -3,14 +3,20 @@ from configparser import ConfigParser
 
 __version__ = "0.1.0"
 
+environments = ConfigParser()
+environments.read("/opt/sixfab/.env")
+environments = environments["pms"]
+
+configs = {
+    "version": __version__,
+    "feeder_interval": int(environments.get("INTERVAL", 10)),
+    "firmware_update_repository": "https://git.ray.kim/sixfab-power/temp-fota-repo.git",
+}
+
 if __name__ == "__main__":
-    configs = ConfigParser()
-    configs.read("/opt/sixfab/.env")
-    configs = configs["pms"]
  
     agent = Agent(
-        configs["TOKEN"],
-        version=__version__,
-        interval=int(configs["INTERVAL"]),
+        environments["TOKEN"],
+        configs=configs
     )
     agent.loop()
