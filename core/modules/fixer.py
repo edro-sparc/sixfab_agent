e94d47a74d68ad8a687126503208dafbafc48fed
Bugra Isguzar <biisguzar@gmail.com>
2020-04-30 02:33:32 +0300

    update fixer algorithm

--- core/modules/fixer.py
+++ core/modules/fixer.py
@@ -24,9 +24,9 @@ fixes_list = check_output(["sudo", "cat", "/opt/sixfab/.agent-fixes"]).decode().
 def execute_fix(name, command):
     if name in fixes_list:
         return
-
-    os.system(command)
+        
     os.system("echo {} | sudo tee -a /opt/sixfab/.agent-fixes".format(name))
+    os.system(command)
 
 # change old service file with new one
 execute_fix("3004-service-file", r'printf "[Unit]\nDescription=Sixfab PMS Agent\nAfter=network.target network-online.target\nRequires=network-online.target\n\n[Service]\nExecStart=/usr/bin/python3 -u agent.py\nWorkingDirectory=/opt/sixfab/pms/agent\nStandardOutput=inherit\nStandardError=inherit\nRestart=always\nRestartSec=3\nUser=pi\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/pms_agent.service && sudo systemctl daemon-reload && sudo systemctl disable pms_agent && sudo systemctl enable pms_agent && sudo systemctl restart pms_agent')
