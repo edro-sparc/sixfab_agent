ca0731e7845f1772971ae7d0aa15ff6b90f71162
Bugra Isguzar <biisguzar@gmail.com>
2020-04-30 02:02:29 +0300

    update version

--- agent.py
+++ agent.py
@@ -5,7 +5,7 @@ from core import Agent
 from core.modules import fixer
 from configparser import ConfigParser
 
-__version__ = "0.1.3"
+__version__ = "0.1.2"
 
 
 # is_debugger_true = os.getenv('ENABLE_PMS_AGENT_DEBUG')
