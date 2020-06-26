43e46315426c24a67d834c8dbe89e57a1eefa5b6
Bugra Isguzar <biisguzar@gmail.com>
2020-06-26 14:02:55 +0200

    updated filename for agen logs

--- core/helpers/logger.py
+++ core/helpers/logger.py
@@ -13,7 +13,7 @@ def initialize_logger():
     logger.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
-    log_file_handler = logging.handlers.TimedRotatingFileHandler(filename=logging_file_path+"agent.log", when="midnight", backupCount=3)
+    log_file_handler = logging.handlers.TimedRotatingFileHandler(filename=logging_file_path+"agent-log", when="midnight", backupCount=3)
     log_file_handler.setFormatter(formatter)
 
     logger.addHandler(log_file_handler)
