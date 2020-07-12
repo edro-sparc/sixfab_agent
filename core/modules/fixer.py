f5c992349b0ea8451153e38755fcf2ffc08c0620
Bugra Isguzar <biisguzar@gmail.com>
2020-07-12 21:57:34 +0300

    updated fixer, added new fix-process

--- core/modules/fixer.py
+++ core/modules/fixer.py
@@ -1,5 +1,5 @@
 import os
-from subprocess import check_output
+from subprocess import check_output, call
 
 if not os.path.exists("/opt/sixfab/.agent-fixes"):
     os.system("sudo touch /opt/sixfab/.agent-fixes")
@@ -11,8 +11,16 @@ def execute_fix(name, command):
         return
         
     os.system("echo {} | sudo tee -a /opt/sixfab/.agent-fixes".format(name))
-    os.system(command)
+    call(command, shell=True, executable='/bin/bash')
 
 # change old service file with new one
 execute_fix("3004-service-file", r'printf "[Unit]\nDescription=Sixfab PMS Agent\nAfter=network.target network-online.target\nRequires=network-online.target\n\n[Service]\nExecStart=/usr/bin/python3 -u agent.py\nWorkingDirectory=/opt/sixfab/pms/agent\nStandardOutput=inherit\nStandardError=inherit\nRestart=always\nRestartSec=3\nUser=pi\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/pms_agent.service && sudo systemctl daemon-reload && sudo systemctl restart pms_agent')
-# execute_fix("10.07.2020-distribution-service", "TODO")
+execute_fix("10.07.2020-distribution-service", r"""pip3 install -U sixfab-power-python-api;API_LOCATION=$(cd /opt/sixfab/pms/api&&sudo git remote show origin);if [[ $API_LOCATION == *"sixfab.com/sixfab-power/api"* ]];then sudo rm -r /opt/sixfab/pms/api;sudo git clone https://github.com/sixfab/power_distribution-service.git /opt/sixfab/pms/api;pip3 install -r /opt/sixfab/pms/api/requirements.txt;sudo touch /etc/systemd/system/sixfab_power_api.service;echo "[Unit]
+Description=Sixfab UPS HAT Distributed API
+
+[Service]
+User=pi
+ExecStart=/usr/bin/python3 /opt/sixfab/pms/api/run_server.py
+
+[Install]
+WantedBy=multi-user.target"|sudo tee /etc/systemd/system/sixfab_power_api.service;sudo systemctl daemon-reload;sudo systemctl enable sixfab_power_api;sudo systemctl start sixfab_power_api;fi""")
